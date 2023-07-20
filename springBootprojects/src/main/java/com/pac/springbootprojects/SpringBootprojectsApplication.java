a59a987ba16bfa8eaff0529afb9a0af776d20406
suchi5533 <suchitw867@gmail.com>
2023-07-20 12:40:26 +0530

    "first commit for spring boot projet"

---
+++ springBootprojects/src/main/java/com/pac/springbootprojects/SpringBootprojectsApplication.java
@@ -0,0 +1,14 @@
+package com.pac.springbootprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootprojectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootprojectsApplication.class, args);
+        System.out.println("Spring boot project is started");
+    }
+
+}
---
+++ springBootprojects/src/test/java/com/pac/springbootprojects/SpringBootprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pac.springbootprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootprojectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
